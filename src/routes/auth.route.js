5b46062ff7fa96c1be51cc37819b31ce99b903d2
Lorenzoo195 <lorenzoactur@gmail.com>
2026-04-27 18:36:19 +0200

    feat: implement authentication route structure with validation and middleware integration

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -5,3 +5,9 @@
 
 const express = require('express');
 const router = express.Router();
+
+// Importamos el Controlador y el Validador
+const authController = require('../controllers/auth.controller');
+const { validateRegister, validateLogin } = require('../validators/auth.validator');
+const { handleValidationErrors } = require('../middlewares/error.middleware'); 
+const { authenticateToken, authorizeRoles } = require('../middlewares/auth.middleware');
