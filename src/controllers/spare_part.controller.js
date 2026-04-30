5906906861f4d3f2ca0adc78d647fb9ebd63a9ff
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2026-04-30 16:41:44 +0200

    feat: add a function to retrieve parts by ID in the Spare Parts CRUD

--- src/controllers/spare_part.controller.js
+++ src/controllers/spare_part.controller.js
@@ -7,7 +7,6 @@
 const sparePartService = require('../services/spare_part.service');
 
 
-
 /**
  * Obtiene piezas por su nombre (coincidencia parcial).
  * @param {Object} req - Objeto Request de Express.
@@ -26,5 +25,27 @@ async function getSparePartsByName(req, res) {
 
     } catch (error) {
         res.status(500).json({ message: 'Error interno del servidor', error: error.message })
+    }
+}
+
+
+/**
+ * Obtiene una pieza específica por su ID.
+ * @param {Object} req - Objeto Request de Express.
+ * @param {Object} res - Objeto Response de Express.
+ */
+async function getSparePartsById(req, res) {
+    try {
+        const { id } = req.params;
+
+        const sparePart = await sparePartService.findSparePartById(id);
+
+        if (!sparePart) {
+            return res.status(404).json({ message: 'La Pieza con ese ID no existe' });
+        }
+        res.json(sparePart);
+
+    } catch (error) {
+        res.status(500).json({ message: 'Error al buscar la pieza', error: error.message })
     }
 }
